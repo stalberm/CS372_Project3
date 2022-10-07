f5f0f53e01a2f904199f3230c6c8196cdb3b776c
Micah Stalberg <stalberm@oregonstate.edu>
2022-10-07 12:44:43 -0700

    Refactors filename extraction into a function

--- webserver.py
+++ webserver.py
@@ -16,6 +16,20 @@ s = socket.socket()
 s.bind(('', int(port)))
 s.listen()
 
+
+def get_requested_file_name(request):
+    #Split request by new lines, first line is the GET
+    try:
+        GET_reqst = request.split("\r\n")[0]
+        #Split GET request and grab the path
+        path = GET_reqst.split()[1]
+        #Strip the filename from the path
+        filename = os.path.split(path)[1]
+    except:
+        #If request was bad, return no filename - leads to 404
+        filename = ''
+    return filename
+
 while True:
     new_conn = s.accept()
     new_socket = new_conn[0]
@@ -28,28 +42,26 @@ while True:
             break
     request = request.decode("ISO-8859-1")
 
-    path = request.split("\r\n")[0].split()[1]
-    print(path)
-
-    filename = os.path.split(path)[1]
-    print(os.path.splitext(filename)[1])
-
+    filename = get_requested_file_name(request)
+    
     try:
         with open(filename) as fp:
+            extension = os.path.splitext(filename)[1]
             data = fp.read()   # Read entire file
             length = len(data.encode("ISO-8859-1"))
-            MIMEType = extMap[os.path.splitext(filename)[1]]
-            response = f"HTTP/1.1 200 OK\r\n\
-Content-Type: {MIMEType}; charset=iso-8859-1\r\n\
-Content-Length: {length}\r\n\
-Connection: close\r\n\r\n" + data
+            MIMEType = extMap[extension]
+            response = f"HTTP/1.1 200 OK\r\n"\
+                        "Content-Type: {MIMEType}; charset=iso-8859-1\r\n"\
+                        "Content-Length: {length}\r\n"\
+                        "Connection: close\r\n\r\n" + data
 
     except:
         # File not found or other error
-        response = f"HTTP/1.1 404 Not Found\r\n\
-Content-Type: text/plain\r\n\
-Content-Length: 13\r\n\
-Connection: close\r\n\r\n\
-404 not found"
+        response = f"HTTP/1.1 404 Not Found\r\n"\
+                    "Content-Type: text/plain\r\n"\
+                    "Content-Length: 13\r\n"\
+                    "Connection: close\r\n\r\n"\
+                    "404 not found"
+
     new_socket.sendall(response.encode("ISO-8859-1"))
     new_socket.close()
